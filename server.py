96e227a1bf29f81a9090be06293c9e838fa53eb1
Daniel Morrissey <djmorrsee@gmail.com>
2014-11-28 20:01:27 -0700

    Began flask djmorrsee.me production

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+# Setup App Server #
+
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def home():
+    return render_template('landing.html')
+
+@app.route('/resume/')
+def resume():
+	return app.send_static_file('pdf/Resume.pdf')
+
+if __name__ == '__main__':
+  app.run(debug=True)
