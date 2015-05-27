0e5112dee0ebc25ac125f1cc02a2840bfe875e62
Daniel Morrissey <djmorrsee@gmail.com>
2015-05-26 20:17:48 -0600

    RESET

--- bin/auth.py
+++
@@ -1,5 +0,0 @@
-from flask.ext.basicauth import BasicAuth
-
-class dj_auth(BasicAuth):
-    def check_credentials(self, username, password):
-        return True
--- bin/loadstrings.py
+++
@@ -1,6 +0,0 @@
-import json;
-
-def load_strings(pagename):
-       with open('static/strings/' + pagename + '.json') as stringfile:
-           
-           return json.load(stringfile)
